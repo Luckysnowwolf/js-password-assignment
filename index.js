c540fdba2289d629597f04d3d26b86d5dae3653a
Luckysnowwolf <179121658+Luckysnowwolf@users.noreply.github.com>
2024-10-22 12:15:03 +0200

    updated index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-let username= ("Fly")
+let username = ("Fly")
 let email = ("star@.se")
-let password= ("Sleepy555")
+let password = ("Sleepy555")
+function isValidAccount(email, username, password) {
+    let capletters = ("A...Z")
+    let letters = ("a...z")
+    let numbers = ("0...1000")
+    let symbols = ("!, @, #, $, %, ^, &, *, (, ), -, _, +, =, {, }, [, ], :, ;, <, >, ., ,, ?, /, |, , `, ~ ")
 
-function isValidEmail(email){
-    return email.includes("@") ? ture : false;
+    return email.includes("@") ? true : false,
+        username.includes(capletters) ? true : false,
+        password.substring(0, 8),
+        password.includes(numbers, capletters, letters, symbols) ? true : false;
 }
-function isValidUsername(username){
-    return username.includes("A...Z") ? ture : false;
-}
-function isValidPassword(password){
-    password.substring(0,8)
-    return password.includes("0...1000", "A...Z","a...z" , "!, @, #, $, %, ^, &, *, (, ), -, _, +, =, {, }, [, ], :, ;, <, >, ., ,, ?, /, |, , `, ~ ") ? ture : false;
 
-}
-console.log(isValidEmail(email))
-console.log(isValidUsername(username))
-console.log(isValidPassword(password))
+console.log(isValidAccount(email))
+console.log(isValidAccount(username))
+console.log(isValidAccount(password))
