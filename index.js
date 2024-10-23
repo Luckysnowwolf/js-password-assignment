d0c8a21dd62bbc2dad94ba6d91e13e19d05547c0
Luckysnowwolf <179121658+Luckysnowwolf@users.noreply.github.com>
2024-10-23 09:36:48 +0200

    updated index,js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-let username = ("Fly")
-let email = ("star@.se")
-let password = ("Sleepy555")
-function isValidAccount(email, username, password) {
-    return email.includes("@") ? true : false;
-}
+let username = prompt("your username")
+let password = prompt("your password")
 
-console.log(isValidAccount(email))
+function isValidAccount(username, password) {
+    if (password.length < 8 ){
+        return false;
+     } else if (password.includes(username)){
+        return false;
+     } else if (password.includes(" ")){
+        return false; 
+     } else { 
+        return true;
+     }
+    }
+
 console.log(isValidAccount(username))
 console.log(isValidAccount(password))
 
@@ -24,12 +31,13 @@ console.log(isValidAccount(password))
 
 
 
-
- /*  let capletters = ("A...Z")
+/*   let capletters = ("A...Z")
     let letters = ("a...z")
     let numbers = ("0...1000")
     let symbols = ("!, @, #, $, %, ^, &, *, (, ), -, _, +, =, {, }, [, ], :, ;, <, >, ., ,, ?, /, |, , `, ~ ")
 
- username.includes(capletters) ? true : false,
+
+return email.includes("@") ? true : false,
+        username.includes(capletters) ? true : false,
         password.substring(0, 8),
-        password.includes(numbers, capletters, letters, symbols) ? true : false; */
+        password.includes(numbers, capletters, letters, symbols) ? true : false;*/
