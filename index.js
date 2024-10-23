0ffc55ba78ed37938918fef4773dad5d6a28aea3
Luckysnowwolf <179121658+Luckysnowwolf@users.noreply.github.com>
2024-10-23 09:05:38 +0200

    updated index.js

--- index.js
+++ index.js
@@ -2,17 +2,34 @@ let username = ("Fly")
 let email = ("star@.se")
 let password = ("Sleepy555")
 function isValidAccount(email, username, password) {
-    let capletters = ("A...Z")
-    let letters = ("a...z")
-    let numbers = ("0...1000")
-    let symbols = ("!, @, #, $, %, ^, &, *, (, ), -, _, +, =, {, }, [, ], :, ;, <, >, ., ,, ?, /, |, , `, ~ ")
-
-    return email.includes("@") ? true : false,
-        username.includes(capletters) ? true : false,
-        password.substring(0, 8),
-        password.includes(numbers, capletters, letters, symbols) ? true : false;
+    return email.includes("@") ? true : false;
 }
 
 console.log(isValidAccount(email))
 console.log(isValidAccount(username))
 console.log(isValidAccount(password))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ /*  let capletters = ("A...Z")
+    let letters = ("a...z")
+    let numbers = ("0...1000")
+    let symbols = ("!, @, #, $, %, ^, &, *, (, ), -, _, +, =, {, }, [, ], :, ;, <, >, ., ,, ?, /, |, , `, ~ ")
+
+ username.includes(capletters) ? true : false,
+        password.substring(0, 8),
+        password.includes(numbers, capletters, letters, symbols) ? true : false; */
